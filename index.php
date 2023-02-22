a69d3d26b15e101edbb0a606f5c1741e7378ba0f
Joe Gibson <joegibso8@gmail.com>
2023-02-21 19:08:18 -0700

    Enhanced window scaling functionality on Featured Meals

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once("header.php");
 		<div id='kitchenBackground'>
 		</div>
 		<div id='homeContainer'>
-			<ul id='recipesToTry'>
+			<ul class= 'column' id='recipesToTry'>
 				<li>
 					<img src="https://bigoven-res.cloudinary.com/image/upload/h_320,w_320,c_fill/spaghetti-8.jpg">
 					<h2>Spaghetti</h2>
@@ -32,8 +32,23 @@ require_once("header.php");
 					<h2>Mojito</h2>
 				</li>
 			</ul>
+			<div class= 'column' id='announcementsColumn'>
+				<ul id='announcements'>
+					<li id="news">
+						<h1><b>ANNOUNCEMENTS</b></h1>
+					</li>
+					<li>
+						<h2>Announcement A</td>
+					</li>
+					<li>
+						<h2>Announcement B</td>
+					</li>
+				</ul>
+				<div id="recipeOfTheMonth">
+					<img src="https://bigoven-res.cloudinary.com/image/upload/h_320,w_320,c_fill/pumpkin-cheesecake-pie-2615a8.jpg">
+				</div>
+			</div>
 		</div>
-		
 		<?php
 			require_once("footer.php");
 		?>
