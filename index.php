d5cf06b75b21437634522064bed0185fea85a259
Joe Gibson <joegibso8@gmail.com>
2023-02-21 17:02:51 -0700

    Redesign of home page

--- index.php
+++ index.php
@@ -8,29 +8,33 @@ require_once("header.php");
 	<body>
 		<div id='kitchenBackground'>
 		</div>
-		<table id='recipesToTry'>
-			<tr>
-				<td class="recipeImage"><img src="https://bigoven-res.cloudinary.com/image/upload/h_320,w_320,c_fill/spaghetti-8.jpg"></td>
-				<td><b>Spaghetti</b></td>
-			</tr>
-			<tr>
-				<td class="recipeImage"><img src="https://bigoven-res.cloudinary.com/image/upload/h_320,w_320,c_fill/quick-ramen-bowl.jpg"></td>
-				<td><b>Ramen</b></td>
-			</tr>
-			<tr>
-				<td class="recipeImage"><img src="https://www.readyseteat.com/sites/g/files/qyyrlu501/files/uploadedImages/img_2070_1452.JPEG"></td>
-				<td><b>Chicken Parmesan</b></td>
-			</tr>
-			<tr>
-				<td class="recipeImage"><img src="https://bigoven-res.cloudinary.com/image/upload/h_320,w_320,c_fill/pumpkin-cheesecake-pie-2615a8.jpg"></td>
-				<td><b>Pumpkin Cheesecake</b></td>
-			</tr>
-			<tr>
-				<td class="recipeImage"><img src="https://bigoven-res.cloudinary.com/image/upload/h_320,w_320,c_fill/drink-mojito-e327ed.jpg"></td>
-				<td><b>Mojito</b></td>
-			</tr>
-		</table>
+		<div id='homeContainer'>
+			<ul id='recipesToTry'>
+				<li>
+					<img src="https://bigoven-res.cloudinary.com/image/upload/h_320,w_320,c_fill/spaghetti-8.jpg">
+					<h2>Spaghetti</h2>
+					<p> test </p>
+				</li>
+				<li>
+					<img src="https://bigoven-res.cloudinary.com/image/upload/h_320,w_320,c_fill/quick-ramen-bowl.jpg">
+					<h2>Ramen</h2>
+				</li>
+				<li>
+					<img src="https://www.readyseteat.com/sites/g/files/qyyrlu501/files/uploadedImages/img_2070_1452.JPEG">
+					<h2>Chicken Parmesan</h2>
+				</li>
+				<li>
+					<img src="https://bigoven-res.cloudinary.com/image/upload/h_320,w_320,c_fill/pumpkin-cheesecake-pie-2615a8.jpg">
+					<h2>Pumpkin Cheesecake</h2>
+				</li>
+				<li>
+					<img src="https://bigoven-res.cloudinary.com/image/upload/h_320,w_320,c_fill/drink-mojito-e327ed.jpg"></td>
+					<h2>Mojito</h2>
+				</li>
+			</ul>
+		</div>
+		<?php
+			require_once("footer.php");
+		?>
 	</body>
 </html>
-<?php
-	require_once("footer.php");
